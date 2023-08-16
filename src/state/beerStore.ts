15f3b76544777dd8aa60493784acd08ce1912a51
Bartosz Jedlinski <b.jedlinski@bewpage.com>
2023-08-16 11:14:11 +0200

    chore: deploy

--- src/state/beerStore.ts
+++ src/state/beerStore.ts
@@ -1,7 +1,6 @@
 import { create } from 'zustand';
 import { fetchBeers as fetchFromApi } from '../api/punkApi';
 
-
 // add page configurator settings for app
 export const pageSettings: {
   pageSize: number;
